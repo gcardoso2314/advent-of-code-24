f1a245098ac69e789c40dd2105b0d3d7bee6ce63
Gerard Cardoso <gerard2314@gmail.com>
2024-12-12 18:05:03 +0000

    d11: optimisations

--- day_11/src/lib.rs
+++ day_11/src/lib.rs
@@ -13,18 +13,37 @@ fn apply_rules(num: &usize) -> Vec<usize> {
     }
 }
 
+fn run_blinks(stones: Vec<usize>, n: usize) -> usize {
+    let mut num_counts: HashMap<usize, usize> = HashMap::new();
+    for &stone in &stones {
+        *num_counts.entry(stone).or_insert(0) += 1;
+    }
+
+    for _ in 0..n {
+        let mut updates: HashMap<usize, usize> = HashMap::new();
+        for (num, count) in num_counts.iter() {
+            let mapping = apply_rules(num);
+            for &n in &mapping {
+                updates
+                    .entry(n)
+                    .and_modify(|c| *c += count)
+                    .or_insert(*count);
+            }
+        }
+        num_counts = updates;
+    }
+
+    num_counts.values().sum()
+}
+
 pub fn process_part_one(input: &str) -> usize {
-    let mut stones: Vec<usize> = input
+    let stones: Vec<usize> = input
         .trim()
         .split(" ")
         .filter_map(|n| n.parse::<usize>().ok())
         .collect();
 
-    for _ in 0..25 {
-        stones = stones.iter().flat_map(|num| apply_rules(num)).collect();
-    }
-
-    stones.len()
+    run_blinks(stones, 25)
 }
 
 pub fn process_part_two(input: &str) -> usize {
@@ -34,37 +53,7 @@ pub fn process_part_two(input: &str) -> usize {
         .filter_map(|n| n.parse::<usize>().ok())
         .collect();
 
-    let mut rule_mappings: HashMap<usize, Vec<usize>> = HashMap::new();
-    let mut num_counts: HashMap<usize, usize> = HashMap::new();
-    for &stone in &stones {
-        *num_counts.entry(stone).or_insert(0) += 1;
-    }
-
-    for _ in 0..75 {
-        let mut updates: HashMap<usize, usize> = HashMap::new();
-        for (num, count) in num_counts.iter() {
-            if let Some(mapping) = rule_mappings.get(num) {
-                for &n in mapping {
-                    updates
-                        .entry(n)
-                        .and_modify(|c| *c += count)
-                        .or_insert(*count);
-                }
-            } else {
-                let mapping = apply_rules(num);
-                for &n in &mapping {
-                    updates
-                        .entry(n)
-                        .and_modify(|c| *c += count)
-                        .or_insert(*count);
-                }
-                rule_mappings.insert(*num, mapping);
-            }
-        }
-        num_counts = updates;
-    }
-
-    num_counts.values().sum()
+    run_blinks(stones, 75)
 }
 
 #[cfg(test)]
@@ -75,7 +64,7 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        assert_eq!(process_part_two(TEST_INPUT), 55312)
+        assert_eq!(process_part_one(TEST_INPUT), 55312)
     }
 
     #[test]
