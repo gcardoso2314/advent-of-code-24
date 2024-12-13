48affa61282ee83f2ec56c3cbe77ddaa4949acff
Gerard Cardoso <gerard2314@gmail.com>
2024-12-13 17:43:22 +0000

    d7: bench

--- day_07/benches/puzzle_bench.rs
+++ day_07/benches/puzzle_bench.rs
@@ -1,14 +1,10 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use day_07::process_part_two;
-use std::fs;
-
-fn run_part_two() {
-    let input = fs::read_to_string("input.txt").expect("error loading the input file");
-    process_part_two(&input);
-}
 
 fn bench_part_two(c: &mut Criterion) {
-    c.bench_function("day_07 part two", |b| b.iter(|| run_part_two()));
+    c.bench_function("day_07 part two", |b| {
+        b.iter(|| process_part_two(include_str!("../input.txt")))
+    });
 }
 
 criterion_group!(benches, bench_part_two);
